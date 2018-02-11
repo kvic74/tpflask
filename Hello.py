b90cee4e0680f7e142cd144c995ef34dac66f43f
kvic74 <vk.vkkv74@gmail.com>
2018-02-11 23:55:25 +0200

    Flask - Variable Rules new def

--- Hello.py
+++ Hello.py
@@ -2,6 +2,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+    """TODO: Docstring for show_blog.
+
+    :p: TODO
+    :returns: TODO
+
+    """
+    return 'Blog Number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+    """TODO: Docstring for revision.
+    :returns: TODO
+
+    """
+    return 'Revision Number %f' % revNo
+
 @app.route('/hello/<name>')
 def hello_world(name):
     """TODO: Docstring for hello_world.
