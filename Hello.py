1dedee14855e2e09c8da734f33e9b13fd0612b87
kvic74 <vk.vkkv74@gmail.com>
2018-02-11 23:35:17 +0200

    Flask - Variable Rules

--- Hello.py
+++ Hello.py
@@ -2,13 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
+@app.route('/hello/<name>')
+def hello_world(name):
     """TODO: Docstring for hello_world.
     :returns: TODO
     """
-    return 'Hello world!'
+    return 'Hello %s!' % name
 
 if __name__ == "__main__":
     app.run()
-
